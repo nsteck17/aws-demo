916908638fc2538cd6c437d94fe71b069e1157f1
Nathan Stechschulte <132308400+nsteck17@users.noreply.github.com>
2024-12-09 22:19:41 -0600

    Updates

--- pages/api/create_user.ts
+++ pages/api/create_user.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from "@/model/db";
-import {scan_table, ScanTableType} from "@/dynamodb";
+
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<ScanTableType>
+    res: NextApiResponse
 ) {
-
-    const rows = await scan_table(req.body.TableName)
-    return res.status(200).json(rows)
+    console.log('1111', req.body);
+    try {
+        const user = await prisma.user.create({ data: req.body });
+        res.status(200).json(user);
+    } catch (e) {
+        res.status(400).json(e);
+    }
 }
